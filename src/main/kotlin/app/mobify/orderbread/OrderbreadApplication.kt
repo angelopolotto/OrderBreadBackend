f734d9edbd1e636b7e3266517d48695a8c28d987
Ângelo Polotto <angelopolotto@yahoo.com.br>
2019-01-18 17:55:14 -0200

    CORS added

--- src/main/kotlin/app/mobify/orderbread/OrderbreadApplication.kt
+++ src/main/kotlin/app/mobify/orderbread/OrderbreadApplication.kt
@@ -2,10 +2,27 @@ package app.mobify.orderbread
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
 
 @SpringBootApplication
 class OrderbreadApplication
 
+//https://spring.io/blog/2015/06/08/cors-support-in-spring-framework
+@Configuration
+class MyConfiguration {
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**")
+            }
+        }
+    }
+}
 fun main(args: Array<String>) {
     runApplication<OrderbreadApplication>(*args)
 }
